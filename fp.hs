039d2ba533a5ca333d080c11c946d1ed49f88cb9
Li Zhang <zhangli@gwu.edu>
2016-05-19 13:26:23 -0400

    list functions

--- fp.hs
+++ fp.hs
@@ -25,3 +25,23 @@ fibonacci :: Integral a => a -> a
 fibonacci 1 = 1
 fibonacci 2 = 2
 fibonacci n = fibonacci (n-1) + fibonacci (n-2)
+
+myOr :: [Bool] -> Bool
+myOr [] = False
+myOr (x:xs) = x || myOr xs
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f [] = False
+myAny f (x:xs) = f x || myAny f xs
+
+myElem :: Eq a => a -> [a] -> Bool
+myElem _ [] = False
+myElem y (x:xs) = y == x || myElem y xs
+
+squish :: [[a]] -> [a]
+squish [] = []
+squish (x:xs) = x ++ squish xs
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap _ [] = []
+squishMap f (x:xs) = f x ++ squishMap f xs
