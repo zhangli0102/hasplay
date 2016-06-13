cb0149fbabc566688e9c9c5b064564e1fbba260c
Li Zhang <zhangli@gwu.edu>
2016-06-13 10:21:57 -0400

    New test script

---
+++ test.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+value1=$(<CONTRACT_VERSION)
+value2=$(<../test/CONTRACT_VERSION)
+echo $value1
+echo $value2
+EXIT=1 #expecting failure
+if [ $value1 -eq $value2 ]; then
+  echo Sucessed!
+  EXIT=0
+else
+  echo Failed.
+fi
+exit $EXIT
+
