e83d02533adeef8da4772a6e5d6478fc9f76577a
Li Zhang <zhangli@gwu.edu>
2016-06-12 22:06:30 -0400

    Edit gg file

--- gg.hs
+++ gg.hs
@@ -20,3 +20,15 @@ quicksort (x:xs) =
     let largesorted = quicksort [n|n<-xs,n>x]
         smallsorted = quicksort [n|n<-xs,n<x]
     in  smallsorted ++ [x] ++ largesorted
+
+data Point = Point Float Float deriving Show
+data Shape = Circle Point Float | Rectangle Point Point deriving Show
+surface :: Shape -> Float
+surface (Circle _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+data Animal = Person { name :: String
+                     , age :: Int
+                     , height :: Float
+                     , weight :: Float
+                     } deriving (Show, Read, Eq) 
