d274ad0d378d034d86631829faa8e212970760cf
Li Zhang <zhangli@gwu.edu>
2016-06-18 18:11:52 -0400

    Short modified

--- io/short.hs
+++ io/short.hs
@@ -1,10 +1,1 @@
-main = do
-    contents <- getContents
-    putStr (short contents)
-
-short :: String -> String
-short input =
-    let raw = lines input
-        second = filter (\x -> length x < 10) raw
-        output = unlines second
-    in  output
+main = interact $ unlines . filter ((<10) . length) . lines
