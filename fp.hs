049d823f2e39f336aa8a1e030e3cc53e33509482
Li Zhang <zhangli@gwu.edu>
2016-06-28 23:45:11 -0400

    Maybe Just Nothing

--- fp.hs
+++ fp.hs
@@ -111,4 +111,13 @@ plum = JamJars "Plum" 7
 apple = JamJars "Apple" 5
 blackberry = JamJars "Blackberry" 10
 
+type Age = Int
+type Name = String
+ 
+data Person = Person Name Age deriving Show
+
+mkPerson :: Name -> Age -> Maybe Person
+mkPerson name age
+  | name /= "" && age >= 0 = Just $ Person name age
+  | otherwise = Nothing
 
