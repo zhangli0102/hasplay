27be6bd16ea1d4999bf8bf41c2a9aec7b887adfa
Li Zhang <zhangli@gwu.edu>
2016-08-10 23:44:27 -0400

    huiwen

--- rw.hs
+++ rw.hs
@@ -27,3 +27,14 @@ mySecond :: [a] -> Maybe a
 mySecond l@(x:xs)
   | length l < 2 = Nothing
   | otherwise = Just (head xs)
+
+length' :: [a] -> Int
+length' [] = 0
+length' (x:xs) = 1 + length' xs
+
+
+hui :: (Num a, Eq a) => [a] -> Bool
+hui [] = True
+hui (x:xs) 
+  | x == last xs = hui.init $ xs
+  | otherwise = False
