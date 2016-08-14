8f52a84262c21d1656a5a93ed4f1be05252ed2b7
Li Zhang <zhangli@gwu.edu>
2016-08-14 14:19:59 -0400

    JValue

---
+++ simpleJSON.hs
@@ -0,0 +1,32 @@
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String, JValue)]
+            | JArray [JValue]
+              deriving (Eq, Ord, Show)
+
+getString :: JValue -> Maybe String
+getString (JString a) = Just a
+getString _ = Nothing
+
+getInt :: JValue -> Maybe Int
+getInt (JNumber a) = Just (truncate a)
+getInt _ = Nothing
+
+getDouble :: JValue -> Maybe Double
+getDouble (JNumber a) = Just a
+getDouble _ = Nothing
+
+getBool :: JValue -> Maybe Bool
+getBool (JBool a) = Just a
+getBool _ = Nothing
+
+getObject (JObject o ) = Just o
+getObject _ = Nothing
+
+getArray(JArray a) = Just a
+getArray _ = Nothing
+
+isNull v = v ==JNull
+
