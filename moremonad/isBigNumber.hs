90069132898cecc5e36a5ff817c4055f65e52b38
zhangli0102 <zhangli@gwu.edu>
2016-08-24 10:58:53 -0700

    applyLog

--- moremonad/isBigNumber.hs
+++ moremonad/isBigNumber.hs
@@ -2,3 +2,7 @@ isBigNumber :: Int -> (Bool, String)
 isBigNumber n
   | n > 9 = (True, "to see if the number is bigger than 9.")
   | otherwise = (False, "to see if the number is bigger than 9.")
+
+applyLog :: (x, String) -> (x -> (y, String)) -> (y, String)
+applyLog (a, log) f = let (x, newlog) = f a in (x, log ++ newlog)
+
