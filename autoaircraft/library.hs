1fe327ec52eb464d5ad422dbacf7edfc40aa85d0
Li Zhang <zhangli@gwu.edu>
2016-08-24 22:54:01 -0400

    new

--- autoaircraft/library.hs
+++ autoaircraft/library.hs
@@ -1,4 +1,9 @@
+module Library where
+
 findLeg :: (Float -> Bool) -> [Float] -> Int
 findLeg f (x:xs)
   | f x = 1
   | otherwise = 1 + findLeg f xs
+
+readFloatList :: [String] -> [Float]
+readFloatList xs = map read xs
