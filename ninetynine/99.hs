ee5d979e88b748530073b5a92ec8c346fa9b80c6
Li Zhang <zhangli@gwu.edu>
2016-09-05 18:19:53 -0400

    symmetric and mirror

--- ninetynine/99.hs
+++ ninetynine/99.hs
@@ -4,3 +4,12 @@ cbbt :: Int -> [Tree String]
 cbbt 0 = [Empty]
 cbbt n = let (q,r) = quotRem (n-1) 2
          in [Branch "zhangli" left right | i <- [q..q+r], left <- cbbt i, right <- cbbt (n-1-i)]
+
+mirror :: (Tree a) -> (Tree a) -> Bool
+mirror Empty Empty = True
+mirror (Branch _ x y) (Branch _ a b) = mirror x b && mirror y a
+mirror _ _ = False
+
+symmetric :: (Tree a) -> Bool
+symmetric Empty = True
+symmetric (Branch _ a b) = mirror a b
